e84362caf3e951a5f4a8588e109114f9dcde7fda
MacAlong <macalongbe2026@hotmail.com>
2026-03-18 14:47:38 +0800

    fix: Update Hilt to 2.51.1 for better KSP compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Navigation
