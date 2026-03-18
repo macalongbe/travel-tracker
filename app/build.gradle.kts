c42aaefaa6dc99c36f419ab8b585f448e62a9a53
MacAlong <macalongbe2026@hotmail.com>
2026-03-18 16:03:54 +0800

    fix: Add Room schema location arg for KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,9 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1") {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
